edcf2c922fd8975ecfdca43dbe71f5f1f2d45173
Imotep92 <Imogyn.nicholas@yahoo.co.uk>
2025-09-01 02:08:04 +0100

    infinite peg spawns
    pegs now spawn infinitely in designated spawn points
    
    going forward, change the array into a list

--- Assets/Scripts/PegSpawnManager.cs
+++ Assets/Scripts/PegSpawnManager.cs
@@ -1,6 +1,78 @@
 using UnityEngine;
 
-public class Peg_Spawn_Manger : MonoBehaviour
+public class PegSpawnManager : MonoBehaviour
 {
-    public Transform[] pegSpawns;
+    [Header("Scripts")]
+    public static PegSpawnManager pegSpawnManagerScript;
+
+
+    public Transform[] pegSpawns;  // FIX, turn ARRAY into a LIST
+    [SerializeField] GameObject pegPrefab;
+    private Vector2 currentSpawn;
+    public bool hasPeg;
+
+
+
+    void Awake()
+    {
+        if (pegSpawnManagerScript != null && pegSpawnManagerScript != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            pegSpawnManagerScript = this;
+        }
+    }
+
+    void Start()
+    {
+        
+
+
+
+
+        // TODO: instatiate each peg onto a spawnpoint
+
+        // foreach (Vector2 currentspawn in GetComponent<Transform>)
+        // {
+        //     Instantiate(pegPrefab, currentSpawn, Quaternion.identity);
+        // }
+
+
+        // currentDestination = _wm.waypoints[Random.Range(0, _wm.waypoints.Length)].position;
+    }
+
+    void FixedUpdate()
+    {
+        currentSpawn = pegSpawnManagerScript.pegSpawns[Random.Range(0, pegSpawnManagerScript.pegSpawns.Length)].position;
+        
+         Instantiate(pegPrefab, currentSpawn, Quaternion.identity);
+
+        // for (int i = 0; i < 1; i++)
+        // {
+        //     pegSpawns.RemoveAt(currentSpawn);
+        // } FIX, turn ARRAY into a LIST
+        
+    }
+
+    
+
+
+    /*
+    
+void SpawnPointPeg()
+{
+    for (int)
+
+
+    for (int i = 0; i < 20; i++)
+    {
+        Instantiate(pegPrefab, randomPos, Quaternion.identity)
+    }
 }
+*/
+
+}
+
+
--- Assets/Scripts/PointPegScript.cs
+++ Assets/Scripts/PointPegScript.cs
@@ -5,9 +5,6 @@ public class PointPegScript : MonoBehaviour
     [SerializeField] GameObject pegPrefab;
     [SerializeField] int pointValue;
     private Rigidbody2D pegRb;
-    private int xRange;
-    private int yRange;
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,25 +18,10 @@ public class PointPegScript : MonoBehaviour
 
 
     }
-
-    private Vector2 RandomSpawnPoint()
-    {
-        float randomXPosition = Random.Range(-xRange, xRange);
-        float randomYPosition = Random.Range(-yRange, yRange);
-
-        Vector2 randomPos = new Vector2(randomXPosition, randomYPosition);
-
-        return randomPos;
-    }
     
-        /*
-        void SpawnPointPeg()
-        {
-            for (int i = 0; i < 20; i++)
-            {
-                Instantiate(pegPrefab, randomPos, Quaternion.identity)
-            }
-        }
-        */
+     //  TODO: ontriggerevent to destroy self
+    //  TODO: point accumulation for player
+
+   
 
 }
